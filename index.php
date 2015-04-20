2be2b9f8749805cefee00380d4107639dbbe88b7
Roger JangKun Kim <rb2stk@gmail.com>
2015-04-20 12:14:32 -0700

    quiz

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript" src="main.js"></script>
+</head>
+<body>
+	<div class="dan">
+		<p>HELLO</p>
+	</div>
+	<div id="iwao">
+		<p>HALLO</p>
+	</div>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#iwao').click(function() {
+        alert("Hey, you clicked on me again");
+    });
+});
+
+$(document).ready(function() {
+    $('.dan').click(function() {
+        alert("Hey, you clicked on me");
+    });
+});
